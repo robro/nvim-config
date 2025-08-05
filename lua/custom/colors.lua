679318133ee8028afec65e658fbd9cd9429b214a
robro <pibborando@gmail.com>
2025-08-05 09:25:16 -0700

    Add default option to colorschemes

--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -1,21 +1,22 @@
 local default = 'dracula'
 
 local colors = {
-  tokyonight = 'folke/tokyonight.nvim',
-  godotcolour = 'mocte4/godotcolour-vim',
-  dracula = 'dracula/vim',
+  tokyonight = { url = 'folke/tokyonight.nvim', default = 'tokyonight-moon' },
+  godotcolour = { url = 'mocte4/godotcolour-vim', default = 'godotcolour' },
+  dracula = { url = 'dracula/vim', default = 'dracula' },
+  rosepine = { url = 'rose-pine/neovim', default = 'rose-pine' },
 }
 
 local color_specs = {}
 
-for name, url in pairs(colors) do
+for id, color in pairs(colors) do
   table.insert(color_specs, {
-    url,
+    color.url,
     lazy = false,
     priority = 1000,
     config = function()
-      if name == default then
-        vim.cmd.colorscheme(name)
+      if id == default then
+        vim.cmd.colorscheme(color.default)
       end
     end,
   })
