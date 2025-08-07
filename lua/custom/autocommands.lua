32c4a83253df8e846939a9ee96e63cfe841ba9d2
robro <pibborando@gmail.com>
2025-08-06 18:01:33 -0700

    Add autocmd for displaying textwidth colorcolumn based on filetype

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -13,3 +13,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Set colorcolumn based on filetype',
+  group = vim.api.nvim_create_augroup('set-colorcolumn', { clear = true }),
+  callback = function()
+    local textwidths = {
+      gdscript = 100,
+      python = 88,
+    }
+    if textwidths[vim.bo.filetype] ~= nil then
+      vim.o.textwidth = textwidths[vim.bo.filetype]
+      vim.o.colorcolumn = '+1'
+    else
+      vim.o.textwidth = 0
+      vim.o.colorcolumn = ''
+    end
+  end,
+})
