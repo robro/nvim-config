0cf10e3303269259192426855c0fd43f079b31a9
robro <pibborando@gmail.com>
2026-01-22 13:20:57 -0800

    Make background transparent

--- init.lua
+++ init.lua
@@ -13,3 +13,8 @@ require 'custom.lazy'
 require('luasnip.loaders.from_lua').load {
   paths = { '~/.config/nvim/lua/snippets/' },
 }
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
