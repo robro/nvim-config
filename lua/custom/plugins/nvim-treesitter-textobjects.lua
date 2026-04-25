b9d054919d00e19236b46eb73ce3ef0b5bfed783
robro <pibborando@gmail.com>
2026-04-25 11:25:44 -0700

    fix(treesitter): remove `ts_repeat_move` which didn't work anyway

--- lua/custom/plugins/nvim-treesitter-textobjects.lua
+++ lua/custom/plugins/nvim-treesitter-textobjects.lua
@@ -70,10 +70,10 @@ return {
       },
     }
 
-    local ts_repeat_move = require 'nvim-treesitter-textobjects.repeatable_move'
+    -- local ts_repeat_move = require 'nvim-treesitter-textobjects.repeatable_move'
 
     -- vim way: ; goes to the direction you were moving.
-    vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+    -- vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+    -- vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
   end,
 }
